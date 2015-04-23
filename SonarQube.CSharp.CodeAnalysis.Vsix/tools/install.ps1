13ff2c52a3e978a664b7cd879dc6fe578882faa1
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-04-23 17:00:54 +0200

    SONARCSANA-34 Fixed the metadata for Nuget and VSIX

--- SonarQube.CSharp.CodeAnalysis.Vsix/tools/install.ps1
+++ SonarQube.CSharp.CodeAnalysis.Vsix/tools/install.ps1
@@ -1,5 +1,10 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
+if ($project.DTE.Version -ne '14.0')
+{
+    throw 'The package can only be installed on Visual Studio 2015 CTP.'
+}
+
 $analyzersPath = join-path $toolsPath "analyzers"
 $analyzersPath = join-path $analyzersPath "C#"
 $analyzerFilePath = join-path $analyzersPath "SonarQube.CSharp.CodeAnalysis.dll"
