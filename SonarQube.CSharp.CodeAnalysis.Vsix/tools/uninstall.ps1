29fb4677e3f3ac7d4e33f5b18cc5b4d9d7ce934f
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-04-24 10:29:34 +0200

    SONARCSANA-34 Revert all changes post 0.7.0-CTP6 changes made to the NuGet package in previous commit

--- SonarQube.CSharp.CodeAnalysis.Vsix/tools/uninstall.ps1
+++ SonarQube.CSharp.CodeAnalysis.Vsix/tools/uninstall.ps1
@@ -1,9 +1,6 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-if ($project.DTE.Version -eq '14.0')
-{
-	$analyzersPath = join-path $toolsPath "analyzers"
-	$analyzersPath = join-path $analyzersPath "C#"
-	$analyzerFilePath = join-path $analyzersPath "SonarQube.CSharp.CodeAnalysis.dll"
-	$project.Object.AnalyzerReferences.Remove($analyzerFilePath)
-}
+$analyzersPath = join-path $toolsPath "analyzers"
+$analyzersPath = join-path $analyzersPath "C#"
+$analyzerFilePath = join-path $analyzersPath "SonarQube.CSharp.CodeAnalysis.dll"
+$project.Object.AnalyzerReferences.Remove($analyzerFilePath)
