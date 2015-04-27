bb677fa422c2fbd85c9eb27f89ffb7dee9b3380f
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-04-27 17:40:28 +0200

    SONARCSANA-10 Rule: "GetHashCode" should not reference mutable fields

---
+++ SonarQube.CSharp.CodeAnalysis/Rules/GetHashCodeMutable.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * SonarQube C# Code Analysis
+ * Copyright (C) 2015 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarQube.CSharp.CodeAnalysis.Helpers;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings.Sqale;
+
+namespace SonarQube.CSharp.CodeAnalysis.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [SqaleConstantRemediation("10min")]
+    [SqaleSubCharacteristic(SqaleSubCharacteristic.DataReliability)]
+    [Rule(DiagnosticId, RuleSeverity, Description, IsActivatedByDefault)]
+    [Tags("bug")]
+    public class GetHashCodeMutable : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S2328";
+        internal const string Description = "\"GetHashCode\" should not reference mutable fields";
+        internal const string MessageFormat = "Remove this use of \"{0}\".";
+        internal const string Category = "SonarQube";
+        internal const Severity RuleSeverity = Severity.Critical;
+        internal const bool IsActivatedByDefault = true;
+
+        internal static DiagnosticDescriptor Rule = 
+            new DiagnosticDescriptor(DiagnosticId, Description, MessageFormat, Category, 
+                RuleSeverity.ToDiagnosticSeverity(), IsActivatedByDefault, 
+                helpLinkUri: "http://nemo.sonarqube.org/coding_rules#rule_key=csharpsquid%3AS2328");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(
+                c =>
+                {
+                    var methodSyntax = (MethodDeclarationSyntax) c.Node;
+                    var methodSymbol = c.SemanticModel.GetDeclaredSymbol(methodSyntax);
+
+                    if (methodSymbol == null ||
+                        methodSyntax.Identifier.Text != "GetHashCode" ||
+                        !methodSyntax.Modifiers.Any(SyntaxKind.OverrideKeyword))
+                    {
+                        return;
+                    }
+
+                    var parentAndSelfTypes = GetParentAndSelfTypes(methodSymbol.ContainingType);
+
+                    var identifiers = methodSyntax.DescendantNodes()
+                        .OfType<IdentifierNameSyntax>();
+
+                    foreach (var identifier in identifiers)
+                    {
+                        var identifierSymbol = c.SemanticModel.GetSymbolInfo(identifier).Symbol as IFieldSymbol;
+
+                        if (identifierSymbol == null ||
+                            identifierSymbol.IsConst ||
+                            identifierSymbol.IsReadOnly ||
+                            !parentAndSelfTypes.Contains(identifierSymbol.ContainingType))
+                        {
+                            continue;
+                        }
+
+                        c.ReportDiagnostic(Diagnostic.Create(Rule, identifier.GetLocation(), identifier.Identifier.Text));
+                    }
+                },
+                SyntaxKind.MethodDeclaration);
+        }
+
+        private static List<INamedTypeSymbol> GetParentAndSelfTypes(INamedTypeSymbol containingType)
+        {
+            var parentChain = new List<INamedTypeSymbol>();
+            var currentType = containingType;
+            while (currentType != null)
+            {
+                parentChain.Add(currentType);
+                currentType = currentType.BaseType;
+            }
+
+            return parentChain;
+        }
+    }
+}
---
+++ SonarQube.CSharp.Test/TestCases/GetHashCodeMutable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class GetHashCodeMutable
+    {
+        public readonly DateTime birthday;
+        public const int Zero = 0;
+        public int age;
+        public string name;
+
+        public override int GetHashCode()
+        {
+            int hash = Zero;
+            hash += age.GetHashCode(); // Noncompliant
+            hash += this.name.GetHashCode(); // Noncompliant
+            hash += this.birthday.GetHashCode();
+            return hash;
+        }
+        public int SomeMethod()
+        {
+            int hash = Zero;
+            hash += this.age.GetHashCode(); 
+            return hash;
+        }
+    }
+}
