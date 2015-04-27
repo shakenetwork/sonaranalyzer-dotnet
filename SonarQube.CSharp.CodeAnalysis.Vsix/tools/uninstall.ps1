dd7d1e81ed86b688b24f27e5ecbd2a47dec08ea6
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-04-27 15:27:48 +0200

    SONARCSANA-68 Improve Nuget package install scripts to fail gracefully in previous versions of Visual Studio

--- SonarQube.CSharp.CodeAnalysis.Vsix/tools/uninstall.ps1
+++ SonarQube.CSharp.CodeAnalysis.Vsix/tools/uninstall.ps1
@@ -1,6 +1,9 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$analyzersPath = join-path $toolsPath "analyzers"
-$analyzersPath = join-path $analyzersPath "C#"
-$analyzerFilePath = join-path $analyzersPath "SonarQube.CSharp.CodeAnalysis.dll"
-$project.Object.AnalyzerReferences.Remove($analyzerFilePath)
+if ($project.DTE.Version -eq '14.0')
+{
+	$analyzersPath = join-path $toolsPath "analyzers"
+	$analyzersPath = join-path $analyzersPath "C#"
+	$analyzerFilePath = join-path $analyzersPath "SonarQube.CSharp.CodeAnalysis.dll"
+	$project.Object.AnalyzerReferences.Remove($analyzerFilePath)
+}
