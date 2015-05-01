30b74941a91fac6d56044dae400a3a48322fe003
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-05-01 09:17:41 +0200

    SONARCSANA-18 Rule: IP addresses should not be hardcoded

---
+++ SonarQube.CSharp.Test/TestCases/HardcodedIpAddress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class HardcodedIpAddress
+    {
+        public HardcodedIpAddress()
+        {
+            string ip = "127.0.0.1"; // Noncompliant
+
+            ip = "300.0.0.0"; // Compliant, not a valid IP
+        }
+    }
+}
