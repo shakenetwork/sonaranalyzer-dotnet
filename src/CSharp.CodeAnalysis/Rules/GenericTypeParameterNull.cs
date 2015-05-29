02835d43ea77bf2e2b2813431aaa6a086b416f22
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-05-29 15:01:17 +0200

    SONARCSANA-82 Generic parameters not constrained to reference types should not be compared to "null"

---
+++ src/CSharp.CodeAnalysis/Rules/GenericTypeParameterNull.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * SonarQube C# Code Analysis
+ * Copyright (C) 2015 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarQube.CSharp.CodeAnalysis.Helpers;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings.Sqale;
+
+namespace SonarQube.CSharp.CodeAnalysis.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [SqaleConstantRemediation("10min")]
+    [SqaleSubCharacteristic(SqaleSubCharacteristic.LogicReliability)]
+    [Rule(DiagnosticId, RuleSeverity, Description, IsActivatedByDefault)]
+    [Tags("bug")]
+    public class GenericTypeParameterNull : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S2955";
+        internal const string Description = "Generic parameters not constrained to reference types should not be compared to \"null\"";
+        internal const string MessageFormat = "Use a comparison to \"default({0})\" instead or add a constraint to \"{0}\" so that it can't be a value type.";
+        internal const string Category = "SonarQube";
+        internal const Severity RuleSeverity = Severity.Critical;
+        internal const bool IsActivatedByDefault = true;
+
+        private readonly ExpressionSyntax nullExpression = SyntaxFactory.LiteralExpression(SyntaxKind.NullLiteralExpression);
+
+        internal static DiagnosticDescriptor Rule = 
+            new DiagnosticDescriptor(DiagnosticId, Description, MessageFormat, Category, 
+                RuleSeverity.ToDiagnosticSeverity(), IsActivatedByDefault, 
+                helpLinkUri: "http://nemo.sonarqube.org/coding_rules#rule_key=csharpsquid%3AS2955");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(
+                c =>
+                {
+                    var equalsExpression = (BinaryExpressionSyntax) c.Node;
+
+                    var leftIsNull = EquivalenceChecker.AreEquivalent(equalsExpression.Left, nullExpression);
+                    var rightIsNull = EquivalenceChecker.AreEquivalent(equalsExpression.Right, nullExpression);
+
+                    if (!(leftIsNull ^ rightIsNull))
+                    {
+                        return;
+                    }
+
+                    var expressionToTypeCheck = leftIsNull ? equalsExpression.Right : equalsExpression.Left;
+                    var typeInfo = c.SemanticModel.GetTypeInfo(expressionToTypeCheck).Type as ITypeParameterSymbol;
+                    if (typeInfo != null &&
+                        !typeInfo.HasReferenceTypeConstraint &&
+                        !typeInfo.ConstraintTypes.OfType<IErrorTypeSymbol>().Any() &&
+                        !typeInfo.ConstraintTypes.Any(typeSymbol =>
+                            typeSymbol.IsReferenceType &&
+                            typeSymbol.TypeKind == TypeKind.Class))
+                    {
+                        var expressionToReportOn = leftIsNull ? equalsExpression.Left : equalsExpression.Right;
+
+                        c.ReportDiagnostic(Diagnostic.Create(Rule, expressionToReportOn.GetLocation(),
+                            typeInfo.Name));
+                    }
+                },
+                SyntaxKind.EqualsExpression);
+        }
+    }
+}
