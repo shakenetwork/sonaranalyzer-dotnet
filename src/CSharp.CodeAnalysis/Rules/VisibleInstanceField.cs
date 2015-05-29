93e2e3a1542047026256ffd96babcd1261dc57c1
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-05-29 14:18:35 +0200

    SONARCSANA-125 Fields should be private

---
+++ src/CSharp.CodeAnalysis/Rules/VisibleInstanceField.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * SonarQube C# Code Analysis
+ * Copyright (C) 2015 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarQube.CSharp.CodeAnalysis.Helpers;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings.Sqale;
+
+namespace SonarQube.CSharp.CodeAnalysis.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [SqaleConstantRemediation("30min")]
+    [SqaleSubCharacteristic(SqaleSubCharacteristic.ArchitectureChangeability)]
+    [Rule(DiagnosticId, RuleSeverity, Description, IsActivatedByDefault)]
+    [Tags("pitfall")]
+    public class VisibleInstanceField : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S2357";
+        internal const string Description = "Fields should be private";
+        internal const string MessageFormat = "Make \"{0}\" private.";
+        internal const string Category = "SonarQube";
+        internal const Severity RuleSeverity = Severity.Major;
+        internal const bool IsActivatedByDefault = false;
+
+        internal static DiagnosticDescriptor Rule =
+            new DiagnosticDescriptor(DiagnosticId, Description, MessageFormat, Category,
+                RuleSeverity.ToDiagnosticSeverity(), IsActivatedByDefault,
+                helpLinkUri: "http://nemo.sonarqube.org/coding_rules#rule_key=csharpsquid%3AS2357");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(Rule); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(
+                c =>
+                {
+                    var fieldDeclaration = (FieldDeclarationSyntax) c.Node;
+                    foreach (var field in fieldDeclaration.Declaration.Variables
+                        .Select(variableDeclaratorSyntax => new
+                        {
+                            Syntax = variableDeclaratorSyntax,
+                            Symbol = c.SemanticModel.GetDeclaredSymbol(variableDeclaratorSyntax) as IFieldSymbol
+                        })
+                        .Where(f => FieldIsRelevant(f.Symbol)))
+                    {
+                        c.ReportDiagnostic(Diagnostic.Create(Rule, field.Syntax.Identifier.GetLocation(),
+                            field.Syntax.Identifier.ValueText));
+                    }
+
+                },
+                SyntaxKind.FieldDeclaration);
+        }
+
+        private static bool FieldIsRelevant(IFieldSymbol fieldSymbol)
+        {
+            return fieldSymbol != null &&
+                   !fieldSymbol.IsStatic &&
+                   !fieldSymbol.IsConst &&
+                   fieldSymbol.DeclaredAccessibility == Accessibility.Public;
+        }
+    }
+}
