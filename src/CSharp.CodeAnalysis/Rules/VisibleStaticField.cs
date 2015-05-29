00d09a7c9d0b648ad6b461ca8b03b59133e3ddd1
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-05-29 13:21:13 +0200

    SONARCSANA-106 Non-constant static fields should not be visible

---
+++ src/CSharp.CodeAnalysis/Rules/VisibleStaticField.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * SonarQube C# Code Analysis
+ * Copyright (C) 2015 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarQube.CSharp.CodeAnalysis.Helpers;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings;
+using SonarQube.CSharp.CodeAnalysis.SonarQube.Settings.Sqale;
+
+namespace SonarQube.CSharp.CodeAnalysis.Rules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [SqaleConstantRemediation("20min")]
+    [SqaleSubCharacteristic(SqaleSubCharacteristic.SynchronizationReliability)]
+    [Rule(DiagnosticId, RuleSeverity, Description, IsActivatedByDefault)]
+    [Tags("pitfall")]
+    public class VisibleStaticField : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S2223";
+        internal const string Description = "Non-constant static fields should not be visible";
+        internal const string MessageFormat = "Change the visibility of \"{0}\" or make it \"static\" or \"readonly\".";
+        internal const string Category = "SonarQube";
+        internal const Severity RuleSeverity = Severity.Major;
+        internal const bool IsActivatedByDefault = true;
+
+        internal static DiagnosticDescriptor Rule =
+            new DiagnosticDescriptor(DiagnosticId, Description, MessageFormat, Category,
+                RuleSeverity.ToDiagnosticSeverity(), IsActivatedByDefault,
+                helpLinkUri: "http://nemo.sonarqube.org/coding_rules#rule_key=csharpsquid%3AS2223");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(Rule); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(
+                c =>
+                {
+                    var fieldDeclaration = (FieldDeclarationSyntax) c.Node;
+                    foreach (var field in fieldDeclaration.Declaration.Variables
+                        .Select(variableDeclaratorSyntax => new
+                        {
+                            Syntax = variableDeclaratorSyntax,
+                            Symbol = c.SemanticModel.GetDeclaredSymbol(variableDeclaratorSyntax) as IFieldSymbol
+                        })
+                        .Where(f => FieldIsRelevant(f.Symbol)))
+                    {
+                        c.ReportDiagnostic(Diagnostic.Create(Rule, field.Syntax.Identifier.GetLocation(),
+                            field.Syntax.Identifier.ValueText));
+                    }
+
+                },
+                SyntaxKind.FieldDeclaration);
+        }
+
+        private static bool FieldIsRelevant(IFieldSymbol fieldSymbol)
+        {
+            return fieldSymbol != null &&
+                   fieldSymbol.IsStatic &&
+                   !fieldSymbol.IsConst &&
+                   !fieldSymbol.IsReadOnly &&
+                   fieldSymbol.DeclaredAccessibility != Accessibility.Private;
+        }
+    }
+}
