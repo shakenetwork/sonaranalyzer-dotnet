ebc1d46a353dd70ab78c5c76af83e3ec28912fd1
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-06-15 13:51:49 +0200

    SONARCSANA-131 Object.ReferenceEquals called with value types

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/ReferenceEqualsOnValueType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class ReferenceEqualsOnValueType
+    {
+        public ReferenceEqualsOnValueType()
+        {
+            var b = object.ReferenceEquals(1, 2); //Noncompliant
+            b = ReferenceEquals(1, 2); //Noncompliant
+            ReferenceEqualsOnValueType.ReferenceEquals(1, new object()); //Noncompliant
+            ReferenceEquals(new object(), new object());
+        }
+    }
+}
