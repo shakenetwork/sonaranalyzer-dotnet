bb879b0be57e4ee9e70ab435d038496dc9334919
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-06-15 13:21:19 +0200

    SONARCSANA-114 Types and "this" should not be used for locking

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/LockOnThisOrType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class LockOnThisOrType
+    {
+        public void MyLockingMethod()
+        {
+            lock (this) // Noncompliant
+            {
+                // ...
+            }
+
+            lock (lockObj)
+            {
+                // ...
+            }
+
+            lock (typeof(LockOnThisOrType)) // Noncompliant
+            {
+                // ...
+            }
+            lock ((new LockOnThisOrType()).GetType()) // Noncompliant
+            {
+                // ...
+            }
+        }
+
+        object lockObj = new object();
+    }
+}
