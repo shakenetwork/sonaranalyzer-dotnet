4257977b720faa8ef91b3bfa2d0a08c7e090de5c
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-06-15 14:13:39 +0200

    SONARCSANA-128 Thread static field has initializer

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/ThreadStaticWithInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class ThreadStaticWithInitializer
+    {
+        public class Foo
+        {
+            [ThreadStatic]
+            public static object PerThreadObject = new object(); // Noncompliant. Will be null in all the threads except the first one.
+
+            [ThreadStatic]
+            public static object _perThreadObject;
+
+            public static object StaticObject = new object();
+        }
+    }
+}
