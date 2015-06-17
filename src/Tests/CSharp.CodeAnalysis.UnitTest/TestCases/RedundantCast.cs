2288fcc6b57fe7f181ac5ef219da54b2f5470f10
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-06-17 11:27:42 +0200

    SONARCSANA-120 Redundant casts should not be used

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/RedundantCast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Diagnostics
+{
+    public class RedundantCast
+    {
+        void foo(long l)
+        {
+            var x = new int[] {1, 2, 3}.Cast<int>(); // Noncompliant
+            x = Enumerable.Cast<int>(new int[] {1, 2, 3}); // Noncompliant
+            x = x.OfType<int>(); //Noncompliant
+            x = x.Cast<int>(); //Noncompliant
+
+            var y = x.OfType<object>();
+
+            var zz = (int) l;
+            int i = 0;
+            var z = (int) i; // Noncompliant
+            z = (Int32) i; // Noncompliant
+
+            var w = (object) i; 
+        }
+    }
+}
