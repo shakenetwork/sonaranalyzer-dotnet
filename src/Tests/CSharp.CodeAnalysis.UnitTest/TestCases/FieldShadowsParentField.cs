1b7ef1cde597380a24e7e56d561c4d219a4673be
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-06-26 11:20:24 +0200

    SONARCSANA-152 Child class members should not shadow parent class members

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/FieldShadowsParentField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tests.TestCases
+{
+    public class Fruit
+    {
+        protected int ripe;
+        protected int flesh;
+
+        // ...
+        private int flesh_color;
+    }
+
+    public class Raspberry : Fruit
+    {
+        private bool ripe;  // Noncompliant
+        private static int FLESH; // Noncompliant
+        private static int FLESH_COLOR; 
+    }
+}
