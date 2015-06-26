2962d73f11a7cbb70e2a9370bdda9e6f12868cc8
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-06-26 14:49:13 +0200

    SONARCSANA-153 Constructors should only call non-overridable methods

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/ConstructorOverridableCall.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public abstract class ParentAbstract
+    {
+        protected ParentAbstract()
+        {
+            DoSomething();  // Noncompliant
+
+            var action = new Action(() => { DoSomething(); });
+        }
+
+        public abstract void DoSomething();
+    }
+
+    public class Parent
+    {
+        public Parent()
+        {
+            DoSomething();  // Noncompliant
+            DoSomething2();
+
+            var action = new Action(() => { DoSomething(); });
+        }
+
+        public virtual void DoSomething() // can be overridden
+        {
+
+        }
+        public void DoSomething2()
+        {
+
+        }
+    }
+
+    public class Child : Parent
+    {
+        private string foo;
+
+        public Child(string foo) // leads to call DoSomething() in Parent constructor which triggers a NullReferenceException as foo has not yet been initialized
+        {
+            this.foo = foo;
+        }
+
+        public override void DoSomething()
+        {
+            Console.WriteLine(this.foo.Length);
+        }
+    }
+}
