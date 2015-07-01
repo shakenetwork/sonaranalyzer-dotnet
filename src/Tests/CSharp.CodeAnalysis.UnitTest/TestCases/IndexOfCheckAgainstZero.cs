7a4d83c706475294a35969117643077d2335ce74
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-07-01 17:40:27 +0200

    SONARCSANA-149 "IndexOf" checks should not be for positive numbers

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/IndexOfCheckAgainstZero.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    class IndexOfCheckAgainstZero
+    {
+        public IndexOfCheckAgainstZero()
+        {
+            string color = "blue";
+            string name = "ishmael";
+
+            List<string> strings = new List<string>();
+            System.Collections.IList stringIList = new List<string>();
+            strings.add(color);
+            strings.add(name);
+            string[] stringArray = strings.ToArray();
+
+            if (stringIList.IndexOf(color) > 0) // Noncompliant
+            {
+                // ...
+            }
+            if (strings.IndexOf(color) > 0) // Noncompliant
+            {
+                // ...
+            }
+            if (0 < name.IndexOf("ish")) // Noncompliant
+            {
+                // ...
+            }
+            if (-1 < name.IndexOf("ish")) 
+            {
+                // ...
+            }
+            if (2 < name.IndexOf("ish")) 
+            {
+                // ...
+            }
+            if (name.IndexOf("ae") > 0) // Noncompliant
+            {
+                // ...
+            }
+            if (Array.IndexOf(stringArray, color) > 0) // Noncompliant
+            { 
+                // ...
+            }
+            if (Array.IndexOf(stringArray, color) >= 0)
+            {
+                // ...
+            }
+        }
+    }
+}
