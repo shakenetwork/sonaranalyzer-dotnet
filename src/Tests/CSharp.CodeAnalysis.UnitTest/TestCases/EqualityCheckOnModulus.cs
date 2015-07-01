aaf72ba8a958daac7a5b044fc69a296336f2e611
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-07-01 17:36:52 +0200

    SONARCSANA-155 Modulus results should not be checked for direct equality

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/EqualityCheckOnModulus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class EqualityCheckOnModulus
+    {
+        public void isOdd(int x)
+        {
+            var y = x%2 == 1; // Noncompliant; if x is negative, x % 2 == -1
+            y = x%2 != -1; // Noncompliant
+            y = 1 == x%2; // Noncompliant
+            y = x%2 != 0;
+            y = Math.Abs(x%2) == 1;
+        }
+    }
+}
