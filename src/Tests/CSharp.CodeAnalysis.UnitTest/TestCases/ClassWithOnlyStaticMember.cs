25c14722da6ba759299285d6aaf09d8e840ddb8a
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-07-01 10:05:56 +0200

    SONARCSANA-163 Utility classes should not have public constructors

---
+++ src/Tests/CSharp.CodeAnalysis.UnitTest/TestCases/ClassWithOnlyStaticMember.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class StringUtils // Noncompliant
+    {
+        public static string Concatenate(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+        public static string Prop { get; set; }
+    }
+
+    public sealed class StringUtils22 //Noncompliant
+    {
+        public static string Concatenate(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+        public static string Prop { get; set; }
+    }
+
+    public class StringUtilsAsBase 
+    {
+        public StringUtilsAsBase() //Noncompliant, should be protected
+        { }
+        public static string Concatenate(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+        public static string Prop { get; set; }
+    }
+
+
+    public class BaseClass //Compliant, has no methods at all
+    { }
+
+    public class StringUtilsDerived : BaseClass
+    {
+        public static string Concatenate(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+        public static string Prop { get; set; }
+    }
+
+    public interface IInterface
+    { }
+    public class StringUtilsIf : IInterface
+    {
+        public static string Concatenate(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+        public static string Prop { get; set; }
+    }
+
+    public static class StringUtils2
+    {
+        public static string Concatenate(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+    }
+
+    public class StringUtils3
+    {
+        protected StringUtils3()
+        {
+        }
+        public static string Concatenate(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+    }
+}
