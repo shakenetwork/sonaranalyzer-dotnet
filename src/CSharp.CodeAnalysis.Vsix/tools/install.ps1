3a660f44d609801d8551b27f6aa4ea5de505f166
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-07-20 13:11:32 +0200

    LINTVS-144 Nuget package should work with multiple DLLs

--- src/CSharp.CodeAnalysis.Vsix/tools/install.ps1
+++ src/CSharp.CodeAnalysis.Vsix/tools/install.ps1
@@ -5,13 +5,16 @@ if ($project.DTE.Version -ne '14.0')
     throw 'The package can only be installed on Visual Studio 2015.'
 }
 
-$analyzersPath = join-path $toolsPath "analyzers"
-$analyzersPath = join-path $analyzersPath "C#"
-$analyzerFilePath = join-path $analyzersPath "SonarLint.dll"
-
 if ($project.Object.AnalyzerReferences -eq $null)
 {
 	throw 'The package cannot be installed as an analyzer reference.'
 }
 
+$analyzersPath = join-path $toolsPath "analyzers"
+$analyzersPath = join-path $analyzersPath "C#"
+
+$analyzerCommonFilePath = join-path $analyzersPath "SonarLint.Common.dll"
+$project.Object.AnalyzerReferences.Add($analyzerCommonFilePath)
+
+$analyzerFilePath = join-path $analyzersPath "SonarLint.dll"
 $project.Object.AnalyzerReferences.Add($analyzerFilePath)
--- src/CSharp.CodeAnalysis.Vsix/tools/uninstall.ps1
+++ src/CSharp.CodeAnalysis.Vsix/tools/uninstall.ps1
@@ -5,8 +5,16 @@ if ($project.DTE.Version -eq '14.0')
 	$analyzersPath = join-path $toolsPath "analyzers"
 	$analyzersPath = join-path $analyzersPath "C#"
 	$analyzerFilePath = join-path $analyzersPath "SonarLint.dll"
+	$analyzerCommonFilePath = join-path $analyzersPath "SonarLint.Common.dll"
 	if ($project.Object.AnalyzerReferences -ne $null)
 	{
-		$project.Object.AnalyzerReferences.Remove($analyzerFilePath)
+		if ($project.Object.AnalyzerReferences -Contains $analyzerFilePath)
+        {
+            $project.Object.AnalyzerReferences.Remove($analyzerFilePath)
+        }
+		if ($project.Object.AnalyzerReferences -Contains $analyzerCommonFilePath)
+        {
+            $project.Object.AnalyzerReferences.Remove($analyzerCommonFilePath)
+        }
 	}	
 }
