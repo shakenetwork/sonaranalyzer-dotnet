724f007fc45bed4ceec27b38785468345d3d32f3
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-07-20 11:12:43 +0200

    Changing the URL of the rule links

--- src/CSharp.CodeAnalysis/Helpers/DiagnosticReportHelper.cs
+++ src/CSharp.CodeAnalysis/Helpers/DiagnosticReportHelper.cs
@@ -46,7 +46,7 @@ namespace SonarQube.CSharp.CodeAnalysis.Helpers
 
         #region Help link
 
-        private const string HelpLinkPattern = "http://dist.sonarsource.com/csharp/rulesdoc/{0}/{1}.html";
+        private const string HelpLinkPattern = "http://vs.sonarlint.org/rules/index.html#version={0}&ruleId={1}";
         internal static string GetHelpLink(this string ruleId)
         {
             var productVersion = FileVersionInfo.GetVersionInfo(typeof(DiagnosticReportHelper).Assembly.Location).ProductVersion;
