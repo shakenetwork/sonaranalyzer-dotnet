d5ced3568dbe7ac16b4b60e7491070967af3e2e0
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-07-27 10:12:13 +0200

    #72 Anonymous delegates should not be used to unsubscribe from Events

---
+++ src/Tests/SonarLint.UnitTest/TestCases/AnonymousDelegateEventUnsubscribe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    class AnonymousDelegateEventUnsubscribe
+    {
+        public delegate void ChangedEventHandler(object sender, EventArgs e);
+        public delegate void ChangedEventHandler2(object sender);
+        public event ChangedEventHandler Changed;
+        public event ChangedEventHandler2 Changed2;
+
+        void Test()
+        {
+            Changed += (obj, args) => { };
+            Changed -= (obj, args) => { }; //Noncompliant
+
+            Changed -= (obj, args) => Console.WriteLine(); // Noncompliant - single statement
+            Changed -= (obj, args) => delegate () { }; // Noncompliant
+            Changed2 -= obj => delegate () { }; // Noncompliant
+
+            ChangedEventHandler x = (obj, args) => { };
+            Changed -= x;
+        }
+    }
+}
