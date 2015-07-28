f8d848522c8667b8f1a7c2a74fa26648667d9c1e
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-07-28 15:46:51 +0200

    #76 Overriden GetHashCode/Equals calls base 'Object.GetHashCode/Equals()'

---
+++ src/Tests/SonarLint.UnitTest/TestCases/GetHashCodeEqualsOverride.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+
+    class GetHashCodeEqualsOverride
+    {
+        private readonly int x;
+        public GetHashCodeEqualsOverride(int x)
+        {
+            this.x = x;
+        }
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() ^ base.GetHashCode(); //Noncompliant
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj); //Noncompliant
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    class GetHashCodeEqualsOverride2
+    {
+        private readonly int x;
+        public GetHashCodeEqualsOverride2(int x)
+        {
+            this.x = x;
+        }
+        public override int GetHashCode()
+        {
+            return x.GetHashCode();
+        }
+    }
+
+    class GetHashCodeEqualsOverride3 : GetHashCodeEqualsOverride2
+    {
+        private readonly int x;
+        public GetHashCodeEqualsOverride3(int x) : base(x)
+        {
+            this.x = x;
+        }
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() ^ base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    class Base
+    { }
+
+    class Derived : Base
+    {
+        public override int GetHashCode()
+        {
+            return base.GetHashCode(); //Noncompliant, calls object.GetHashCode()
+        }
+    }
+}
