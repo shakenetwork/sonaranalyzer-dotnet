3f307a99f0d74fdbccadbad25e62b2492603b989
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-09-01 13:37:14 +0200

    Issue #174 Trivial properties should be auto-implemented

---
+++ src/Tests/SonarLint.UnitTest/TestCases/PropertyToAutoProperty.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class MyAttribute : Attribute { }
+
+    public class PropertyToAutoProperty
+    {
+        static public implicit operator int (PropertyToAutoProperty x)
+        {
+            return 1;
+        }
+        static public implicit operator PropertyToAutoProperty(int x)
+        {
+            return null;
+        }
+
+        private int field;
+        public PropertyToAutoProperty Prop //Compliant
+        {
+            get { return field; }
+            set { field = value; }
+        }
+
+        [My()]
+        private int field2;
+        public int Prop2 //Compliant
+        {
+            get { return field2; }
+            set { field2 = value; }
+        }
+
+        private string _make;
+        public string Make // Noncompliant
+        {
+            get { return _make; }
+            set { this._make = value; }
+        }
+
+        PropertyToAutoProperty instance = new PropertyToAutoProperty();
+        public string Make5 // Compliant
+        {
+            get { return _make; }
+            set { instance._make = value; }
+        }
+
+        private static string _make3;
+        public static string Make3 // Noncompliant
+        {
+            get { return _make3; }
+            set { PropertyToAutoProperty._make3 = value; }
+        }
+
+        public static string Make4 // Compliant
+        {
+            get { return _make3; }
+            set { PropertyToAutoProperty._make3 += value; }
+        }
+
+        public string Make4 // Compliant, returns a static field
+        {
+            get { return _make3; }
+            set { _make3 = value; }
+        }
+
+        public string Make2
+        {
+            get { return _make; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new SomeException();
+                }
+                _make = value;
+            }
+        }
+    }
+}
