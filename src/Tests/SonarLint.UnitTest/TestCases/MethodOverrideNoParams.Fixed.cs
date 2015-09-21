cd0e76f42dfc9ae9a286ad31ddec8630c6a8be0f
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-09-21 16:42:06 +0200

    Issue #374 "params" should be used on overrides

---
+++ src/Tests/SonarLint.UnitTest/TestCases/MethodOverrideNoParams.Fixed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    abstract class Base
+    {
+        public virtual void Method(params int[] numbers)
+        {
+        }
+        public virtual void Method(string s,
+            params int[] numbers)
+        {
+        }
+        public abstract void Method(string s, string s1,
+            params int[] numbers);
+    }
+    abstract class Derived : Base
+    {
+        public override void Method(params int[] numbers) // Noncompliant, the params is missing.
+        {
+        }
+        public override void Method(string s,
+            params int[] numbers) // Noncompliant
+        {
+        }
+        public override void Method(string s, string s1,
+            params int[] numbers) // Noncompliant
+        { }
+    }
+
+    abstract class Derived2 : Base
+    {
+        public override void Method(params int[] numbers)
+        {
+        }
+    }
+}
