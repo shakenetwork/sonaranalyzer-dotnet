1790b6aef6bc6e7f7abee2c86011afe0fdd78930
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-10-16 14:38:40 +0200

    Issue #484 "IsNot" should be used instead of "Not ... Is ..."

---
+++ src/Tests/SonarLint.UnitTest/TestCases/NegatedIsExpression.Fixed.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+    Sub Main()
+        Dim a = "a" IsNot Nothing ' Noncompliant
+        a = "a" IsNot ' Noncompliant
+            Nothing 'some comment
+        a = "a" IsNot Nothing ' Compliant
+    End Sub
+End Module
---
+++ src/Tests/SonarLint.UnitTest/TestCases/NegatedIsExpression.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+    Sub Main()
+        Dim a = Not "a" Is Nothing ' Noncompliant
+        a = Not "a" Is ' Noncompliant
+            Nothing 'some comment
+        a = "a" IsNot Nothing ' Compliant
+    End Sub
+End Module
