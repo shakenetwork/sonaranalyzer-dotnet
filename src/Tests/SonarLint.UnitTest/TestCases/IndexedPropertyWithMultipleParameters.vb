9091724850f666f2ac0b6e6745584f9d3147340f
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-10-16 09:07:46 +0200

    Issue #479 Indexed properties with more than one parameter should not be used

---
+++ src/Tests/SonarLint.UnitTest/TestCases/IndexedPropertyWithMultipleParameters.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+    ReadOnly Property Sum(ByVal a As Integer, ByVal b As Integer) ' Noncompliant
+        Get
+            Return a + b
+        End Get
+    End Property
+
+    ReadOnly Property Prop As Integer
+        Get
+            Return 42
+        End Get
+    End Property
+
+    Function Sum3(ByVal a As Integer, ByVal b As Integer)          ' Compliant
+        Return a + b
+    End Function
+End Module
