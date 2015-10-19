6877dd9ab7ec2fa368ad3b454715459b2f9d8d72
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-10-19 12:07:06 +0200

    Issue #481 Array literals should be used instead of array creation expressions

---
+++ src/Tests/SonarLint.UnitTest/TestCases/ArrayCreationLongSyntax.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+    Sub Main()
+        Dim foo = New String() {"a", "b", "c"} ' Noncompliant
+        foo = New String() {} ' Compliant
+        Dim foo2 = {}
+        foo2 = {"a", "b", "c"}
+        Dim foo3 = New A() {New B()} ' Compliant
+        foo3 = New A() {New B(), New A()} ' Noncompliant
+    End Sub
+
+End Module
+Class A
+
+End Class
+Class B
+    Inherits A
+
+End Class
