d5cf4936a8c3025d0aa87b606290d364d0d21b0f
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-10-19 16:54:27 +0200

    Issue #474 End statements should not be used

---
+++ src/Tests/SonarLint.UnitTest/TestCases/EndStatementUsage.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+    Sub Print(ByVal str As String)
+        Try
+
+            End        ' Noncompliant
+            Dim a As Integer
+
+        Finally
+            ' do something important here
+
+        End Try
+    End Sub
+End Module
