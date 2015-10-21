236e134d1843e4cd9c5644f9144de1de0c8fbc5b
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-10-21 15:12:26 +0200

    Issue #493 Public methods should not have multidimensional array parameters

---
+++ src/SonarLint/Rules/PublicMethodWithMultidimensionalArrayBase.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * SonarLint for Visual Studio
+ * Copyright (C) 2015 SonarSource
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarLint.Common;
+using SonarLint.Helpers;
+
+namespace SonarLint.Rules.Common
+{
+    public abstract class PublicMethodWithMultidimensionalArrayBase : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S2368";
+        internal const string Title = "Public methods should not have multidimensional array parameters";
+        internal const string Description =
+            "Exposing methods with multidimensional array parameters require developers to have advanced knowledge about the language in " +
+            "order to be able to use them. Moreover, what exactly to pass to such parameters is not intuitive. Therefore, such methods " +
+            "should not be exposed, but can be used internally.";
+        internal const string MessageFormat = "Make this method private or simplify its parameters to not use multidimensional arrays.";
+        internal const string Category = "SonarLint";
+        internal const Severity RuleSeverity = Severity.Major;
+        internal const bool IsActivatedByDefault = true;
+
+        internal static readonly DiagnosticDescriptor Rule =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category,
+                RuleSeverity.ToDiagnosticSeverity(), IsActivatedByDefault,
+                helpLinkUri: DiagnosticId.GetHelpLink(),
+                description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+    }
+
+    public abstract class PublicMethodWithMultidimensionalArrayBase<TLanguageKindEnum, TMethodSyntax> : PublicMethodWithMultidimensionalArrayBase
+        where TLanguageKindEnum : struct
+        where TMethodSyntax: SyntaxNode
+    {
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                c =>
+                {
+                    var method = (TMethodSyntax)c.Node;
+                    var methodSymbol = c.SemanticModel.GetDeclaredSymbol(method) as IMethodSymbol;
+
+                    if (methodSymbol != null &&
+                        IsMethodChangeable(methodSymbol) &&
+                        IsMethodPublic(methodSymbol) &&
+                        MethodHasMultidimensionalArrayParameters(methodSymbol))
+                    {
+                        var identifier = GetIdentifier(method);
+                        c.ReportDiagnostic(Diagnostic.Create(Rule, identifier.GetLocation()));
+                    }
+                },
+                SyntaxKindsOfInterest.ToArray());
+        }
+
+        private static bool MethodHasMultidimensionalArrayParameters(IMethodSymbol methodSymbol)
+        {
+            return methodSymbol.Parameters
+                .Select(param => param.Type as IArrayTypeSymbol)
+                .Where(type => type != null)
+                .Any(type => type.Rank > 1 || type.ElementType is IArrayTypeSymbol);
+        }
+
+        private static bool IsMethodPublic(ISymbol method)
+        {
+            var symbol = method;
+            while (symbol != null &&
+                symbol.DeclaredAccessibility == Accessibility.Public)
+            {
+                symbol = symbol.ContainingSymbol;
+            }
+
+            return symbol == null || symbol.DeclaredAccessibility == Accessibility.NotApplicable;
+        }
+
+        protected abstract SyntaxToken GetIdentifier(TMethodSyntax method);
+
+        public abstract ImmutableArray<TLanguageKindEnum> SyntaxKindsOfInterest { get; }
+
+        private static bool IsMethodChangeable(IMethodSymbol methodSymbol)
+        {
+            if (methodSymbol.IsOverride)
+            {
+                return false;
+            }
+
+            return !methodSymbol.ContainingType
+                .AllInterfaces
+                .SelectMany(@interface => @interface.GetMembers().OfType<IMethodSymbol>())
+                .Any(method => methodSymbol.Equals(methodSymbol.ContainingType.FindImplementationForInterfaceMember(method)));
+        }
+    }
+}
---
+++ src/Tests/SonarLint.UnitTest/TestCases/PublicMethodWithMultidimensionalArray.vb
@@ -0,0 +1,46 @@
+﻿Imports System
+
+Namespace Tests.Diagnostics
+    Public Interface IFace
+        Sub Method6(a As Integer()()) ' Noncompliant
+    End Interface
+
+    Public Class Base
+        MustOverride Sub Method5(a As Integer()()) ' Noncompliant
+    End Class
+
+    Public Class PublicMethodWithMultidimensionalArray
+        Implements Base, IFace
+        Public Sub Method(a As Integer)
+        End Sub
+        Public Sub MethodX(a As Integer())
+        End Sub
+
+        Public Sub Method1(a As Integer()()) ' Noncompliant
+        End Sub
+        Sub Method2(a As Integer()()) ' Noncompliant
+        End Sub
+        Sub Method2b(a As Integer(,)) ' Noncompliant
+        End Sub
+
+        Private Sub Method3(a As Integer()())
+        End Sub
+
+        Function Method4(a As Integer()()) As Integer ' Noncompliant
+            Return 1
+        End Function
+
+        Overrides Sub Method5(a As Integer()()) ' Compliant
+        End Sub
+
+        Private Sub Method6(a As Integer()()) Implements IFace.Method6 ' Compliant, interface implementation
+            Throw New NotImplementedException()
+        End Sub
+    End Class
+    Friend Class Other
+
+        Public Sub Method1(a As Integer()()) ' Compliant
+        End Sub
+
+    End Class
+End Namespace
