34adb83c4bef6eafce120d97ba35904fc78ed3dd
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-11-02 16:43:49 +0100

    Issue #472 Event names should not have 'Before' or 'After' as a prefix or suffix

---
+++ src/Tests/SonarLint.UnitTest/TestCases/EventNameContainsBeforeOrAfter.vb
@@ -0,0 +1,20 @@
+﻿Class Foo
+    Event BeforeClose() ' Noncompliant
+    Event afterClose()  ' Noncompliant
+    Event Closing()
+    Public Custom Event BeforeClose2 As EventHandler ' Noncompliant
+        AddHandler(ByVal value As EventHandler)
+            Me.Events.AddHandler(Me.EventServerChange, value)
+        End AddHandler
+
+
+        RemoveHandler(ByVal value As EventHandler)
+            Me.Events.RemoveHandler(Me.EventServerChange, value)
+        End RemoveHandler
+
+
+        RaiseEvent(ByVal sender As Object, ByVal e As System.EventArgs)
+            CType(Me.Events(Me.EventServerChange), EventHandler).Invoke(sender, e)
+        End RaiseEvent
+    End Event
+End Class
