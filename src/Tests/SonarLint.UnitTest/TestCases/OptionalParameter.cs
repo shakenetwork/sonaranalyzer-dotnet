25d350d0892fb3d8146527bddfc7766ec9996c66
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-11-02 10:58:50 +0100

    Issue #488 Optional arguments should not be used

---
+++ src/Tests/SonarLint.UnitTest/TestCases/OptionalParameter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class OptionalParameter
+    {
+        public OptionalParameter(int i = 0, // Noncompliant
+            int j = 1) // Noncompliant
+        {
+        }
+        public OptionalParameter()
+        {
+        }
+        private OptionalParameter(int i = 0) // Compliant, private
+        {
+        }
+    }
+}
