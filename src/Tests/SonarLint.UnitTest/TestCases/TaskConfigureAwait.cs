af8c74b4d254d544db89c9b17e0d27c3c67dad48
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-11-23 12:01:13 +0100

    Issue #360 "ConfigureAwait(false)" should be used

---
+++ src/Tests/SonarLint.UnitTest/TestCases/TaskConfigureAwait.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tests.Diagnostics
+{
+    public class TaskConfigureAwait
+    {
+        public async void Test()
+        {
+            await Task.Delay(1000); // Noncompliant
+            await Task.Delay(1000).ConfigureAwait(false); // Compliant
+            await Task.Delay(1000).ConfigureAwait(true); // Compliant, we assume that there is a reason to explicitly specify context switching
+
+            var t = Task.Delay(1000);
+        }
+    }
+}
