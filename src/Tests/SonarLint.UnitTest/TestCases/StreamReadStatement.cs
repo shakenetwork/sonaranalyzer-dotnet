3df2516ea2c627bdd6266afe68084d51ee884ad3
Tamas Vajk <tamas.vajk@sonarsource.com>
2015-11-24 14:29:30 +0100

    Issue #70 The value returned from a stream read should be checked

---
+++ src/Tests/SonarLint.UnitTest/TestCases/StreamReadStatement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Tests.Diagnostics
+{
+    public class StreamReadStatement
+    {
+        public StreamReadStatement(string fileName)
+        {
+            using (var stream = File.Open(fileName, FileMode.Open))
+            {
+                var result = new byte[stream.Length];
+                stream.Read(result, 0, (int)stream.Length); // Noncompliant
+                var l = stream.Read(result, 0, (int)stream.Length);
+                stream.ReadAsync(result, 0, (int)stream.Length); // Noncompliant
+                await stream.ReadAsync(result, 0, (int)stream.Length); // Noncompliant
+                stream.Write(result, 0, (int)stream.Length);
+            }
+        }
+    }
+}
