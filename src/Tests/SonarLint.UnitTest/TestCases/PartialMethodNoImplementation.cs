c4684e714f96ace69b24a483aa7261b6507ba86d
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-02-05 11:12:21 +0100

    SLVS-779 S3251 - Implementations should be provided for "partial" methods

---
+++ src/Tests/SonarLint.UnitTest/TestCases/PartialMethodNoImplementation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Diagnostics
+{
+    public abstract partial class PartialMethodNoImplementation
+    {
+        partial void Method(); //Noncompliant
+
+        void OtherM()
+        {
+            Method(); //Noncompliant. Will be removed.
+            OkMethod();
+            OkMethod2();
+            M();
+        }
+
+        partial void OkMethod();
+        partial void OkMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        partial void OkMethod2()
+        {
+            throw new NotImplementedException();
+        }
+        partial void OkMethod2();
+
+        public abstract void M();
+    }
+}
