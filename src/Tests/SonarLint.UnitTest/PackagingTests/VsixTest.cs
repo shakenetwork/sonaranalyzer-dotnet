b6bf49a73555cf0c1fbe8cd7ba25a498fa0eab32
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-02-10 08:48:42 +0100

    Increasing expected VSIX size

--- src/Tests/SonarLint.UnitTest/PackagingTests/VsixTest.cs
+++ src/Tests/SonarLint.UnitTest/PackagingTests/VsixTest.cs
@@ -33,10 +33,10 @@ namespace SonarLint.UnitTest.PackagingTests
             const string vsixFileName = "SonarLint.vsix";
 #if DEBUG
             const string pathEnding = @"bin\Debug";
-            const int approxFileSize = 727 * 1024;
+            const int approxFileSize = 801 * 1024;
 #else
             const string pathEnding = @"bin\Release";
-            const int approxFileSize = 241 * 1024;
+            const int approxFileSize = 264 * 1024;
 #endif
 
             var currentDirectory = Directory.GetCurrentDirectory();
