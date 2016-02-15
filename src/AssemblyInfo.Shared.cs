a89e31300c81fa1dddd3f3c26ebb5702f80ecad9
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-02-15 13:34:51 +0100

    Changing version number to 1.9.0

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -21,9 +21,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyInformationalVersion("1.9.0")]
 
 [assembly: AssemblyConfiguration("")]
 
