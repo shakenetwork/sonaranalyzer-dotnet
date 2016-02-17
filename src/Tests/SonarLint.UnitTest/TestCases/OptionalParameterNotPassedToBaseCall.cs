edb6c063e72a71a361f9b6fd5b47bb6dbda8af02
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-02-17 16:28:06 +0100

    SLVS-800 S3466 - Optional parameters should be passed to "base" calls

---
+++ src/Tests/SonarLint.UnitTest/TestCases/OptionalParameterNotPassedToBaseCall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class BaseClass
+    {
+        public virtual void MyMethod(int j, int i = 1) { }
+        public virtual void MyMethod2(int i = 1) { }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public override void MyMethod(int j, int i = 1)
+        {
+            ; ; ;
+            base.MyMethod(1); // Noncompliant; caller's value is ignored
+            base.MyMethod(j); // Noncompliant; caller's value is ignored
+            base.MyMethod2();
+            this.MyMethod(1);
+        }
+    }
+}
