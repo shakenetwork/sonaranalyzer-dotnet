04d54799cc2aa22892edcf6b3de1a327b5dbbf14
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-03-15 09:11:40 +0100

    SLVS-827 S3458 - Empty "case" clauses that fall through to the "default" should be omitted

---
+++ src/Tests/SonarLint.UnitTest/TestCases/SwitchCaseFallsThroughToDefault.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tests.Diagnostics
+{
+    public class SwitchCaseFallsThroughToDefault
+    {
+        public SwitchCaseFallsThroughToDefault(char ch)
+        {
+            switch (ch)
+            {
+                case 'b':
+                    handleB();
+                    break;
+                case 'c':  // Noncompliant
+                default:
+                    handleTheRest();
+                    break;
+                case 'a':
+                    handleA();
+                    break;
+            }
+
+            switch (ch)
+            {
+                case 'b':
+                    handleB();
+                    break;
+                case 'c':  // Noncompliant
+                case 'e':  // Noncompliant
+                default:
+                    handleTheRest();
+                    break;
+                case 'a':
+                    handleA();
+                    break;
+            }
+
+            switch (ch)
+            {
+                case 'b':
+                    handleB();
+                    break;
+                case 'c':  // Noncompliant
+                default:
+                case 'e':  // Noncompliant
+                    handleTheRest();
+                    break;
+                case 'a':
+                    handleA();
+                    break;
+            }
+        }
+    }
+}
