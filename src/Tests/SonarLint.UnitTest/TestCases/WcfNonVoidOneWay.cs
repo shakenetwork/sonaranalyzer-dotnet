e56f5dbb1f245d409a03a10a71ecce0d1f06e3b5
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-04-11 15:01:55 +0200

    SLVS-854 S3598 - One-way "OperationContract" methods should have "void" return type

---
+++ src/Tests/SonarLint.UnitTest/TestCases/WcfNonVoidOneWay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Tests.Diagnostics
+{
+    public class MyAttribute : Attribute
+    {
+        public bool isOneWay { get; set; }
+    }
+
+    [ServiceContract]
+    interface IMyService1
+    {
+        [OperationContract]
+        int MyServiceMethod();
+
+        [OperationContract(IsOneWay = true)]
+        int MyServiceMethod2(); // Noncompliant
+
+        [OperationContract(IsOneWay = false)]
+        int MyServiceMethod3();
+
+        [OperationContract(IsTerminating = true)]
+        [My(IsOneWay = true)]
+        int MyServiceMethod4();
+
+        [OperationContract(IsOneWay = "mistake")]
+        int MyServiceMethod5();
+    }
+}
