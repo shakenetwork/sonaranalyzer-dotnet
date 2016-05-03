9a44763dca604e69f23a0cfcfa90f3d9f5e08256
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-05-03 11:20:09 +0200

    SLVS-877 S3610 - Nullable type comparison should not be redundant

---
+++ src/Tests/SonarLint.UnitTest/TestCases/RedundantNullableTypeComparison.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public class MyClass
+    {
+        public Type GetType()
+        {
+            // some other implementation
+            return null;
+        }
+    }
+
+    public class RedundantNullableTypeComparison
+    {
+        public RedundantNullableTypeComparison()
+        {
+            int? nullable = 42;
+            bool comparison = nullable.GetType() == typeof(Nullable<int>); // Noncompliant, always false
+            comparison = nullable.GetType() != typeof(Nullable<int>); // Noncompliant, always true
+
+            comparison = new MyClass().GetType() != typeof(Nullable<int>);
+            comparison = 42.GetType() != typeof(int);
+            comparison = 42.ToString() != typeof(int);
+            comparison = new object() != typeof(Nullable<int>);
+        }
+    }
+}
