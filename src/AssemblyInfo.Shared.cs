eaa4a7c0d8449d2cb73b4bbb933df30b98ec7e52
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-08-31 10:37:41 +0200

    Change version to 1.18.0

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -21,9 +21,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.17.0.0")]
-[assembly: AssemblyFileVersion("1.17.0.0")]
-[assembly: AssemblyInformationalVersion("1.17.0")]
+[assembly: AssemblyVersion("1.18.0")]
+[assembly: AssemblyFileVersion("1.18.0.0")]
+[assembly: AssemblyInformationalVersion("1.18.0")]
 
 [assembly: AssemblyConfiguration("")]
 
