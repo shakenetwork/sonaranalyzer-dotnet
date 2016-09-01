10c488d43145a912b711dc5fe67cfbd8d0c8cc7c
Tom <thomas.verin@sonarsource.com>
2016-09-01 11:00:52 +0200

    Fix QA env variables

--- qa.ps1
+++ qa.ps1
@@ -3,15 +3,15 @@
 & $env:MSBUILD_PATH /p:configuration=Release /p:DeployExtension=false /p:ZipPackageCompressionLevel=normal /v:m
 
 #download nuget package
-$password = convertto-securestring -String "$REPOX_QAPUBLICADMIN_PASSWORD" -AsPlainText -Force
-$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $REPOX_QAPUBLICADMIN_USERNAME, $password
+$password = convertto-securestring -String "$env:REPOX_QAPUBLICADMIN_PASSWORD" -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $env:REPOX_QAPUBLICADMIN_USERNAME, $password
 $ARTIFACTORY_SRC_REPO="sonarsource-nuget-qa"
-$url = "$ARTIFACTORY_URL/$ARTIFACTORY_SRC_REPO/$ARTIFACT"
+$url = "$env:ARTIFACTORY_URL/$ARTIFACTORY_SRC_REPO/$ARTIFACT"
 Invoke-WebRequest -Uri $url -Credential $cred
 
 #unzip nuget package
-$zipName=$ARTIFACT.Substring(0, $ARTIFACT.LastIndexOf('.'))+".zip"
-mv $ARTIFACT $zipName
+$zipName=$env:ARTIFACT.Substring(0, $env:ARTIFACT.LastIndexOf('.'))+".zip"
+mv $env:ARTIFACT $zipName
 $shell_app=new-object -com shell.application
 $currentdir=(Get-Item -Path ".\" -Verbose).FullName
 $destination = $shell_app.NameSpace($currentdir)
@@ -22,9 +22,6 @@ $destination.CopyHere($zip_file.Items())
 mv analyzers\*.dll src\SonarLint.CSharp\bin\Release
 
 #run tests
-
-C:\PROGRA~2\MICROS~2.0\Common7\IDE\COMMON~1\MICROS~1\TESTWI~1\vstest.console.exe 
-
 & $env:VSTEST_PATH .\src\Tests\SonarLint.SonarQube.Integration.UnitTest\bin\Release\SonarLint.SonarQube.Integration.UnitTest.dll
 & $env:VSTEST_PATH .\src\Tests\SonarLint.UnitTest\bin\Release\SonarLint.UnitTest.dll
  
