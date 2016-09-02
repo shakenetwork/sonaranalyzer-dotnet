519f746dd6a18cef3f8d1f55cf802cb53c41f38a
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-09-02 15:49:20 +0200

    SAVB-21 Add naming rule for non-private fields

---
+++ src/Tests/SonarLint.UnitTest/TestCases/PublicFieldName.vb
@@ -0,0 +1,10 @@
+﻿Class Foo
+    Public Shared ReadOnly Foo As Integer  ' Compliant
+
+    Public Shared foo2 As Integer ' Noncompliant
+'                 ^^^^
+    Public Shared FooFooFFFoooFF As Integer
+    Friend ReadOnly FooFooFFFFoooFF As Integer ' Noncompliant
+    Protected FooFooFFFFoooFFF As Integer ' Noncompliant
+
+End Class
