632e80322fe6e85356c6cc451a022552a926f557
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-09-02 15:49:03 +0200

    SAVB-7 Add naming rule for Private fields

---
+++ src/Tests/SonarLint.UnitTest/TestCases/PrivateFieldName.vb
@@ -0,0 +1,15 @@
+﻿Class Foo
+    Private ReadOnly Foo As Integer  ' Noncompliant
+'                    ^^^
+
+    Private ReadOnly foo2 As Integer
+    Private ReadOnly s_fooFooFFFoooFF
+    Private ReadOnly sfooFooFFFoooFF
+    Private ReadOnly sFooFooFFFooooFF
+    Private ReadOnly _fooFooFFFoooFF, __fooFooFFFoooFF As Integer ' Noncompliant
+'                                     ^^^^^^^^^^^^^^^^
+
+    Private Shared _fooFooFFFFoooFF As Integer ' Noncompliant
+    Private Shared _fooFooFFFoooF As Integer ' Noncompliant
+
+End Class
