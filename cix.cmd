59b9c099f33e9d2eac09e4c531f47d33fb34fea4
Tom <thomas.verin@sonarsource.com>
2016-09-16 10:54:45 +0200

    QA: add error handling in cmd scripts

--- cix.cmd
+++ cix.cmd
@@ -1,1 +1,5 @@
 powershell -ExecutionPolicy Unrestricted -File cix.ps1
+if errorlevel 1 (
+   echo Failure Reason Given is %errorlevel%
+   exit /b %errorlevel%
+)
--- qa.cmd
+++ qa.cmd
@@ -1,1 +1,5 @@
 powershell -ExecutionPolicy Unrestricted -File qa.ps1
+if errorlevel 1 (
+   echo Failure Reason Given is %errorlevel%
+   exit /b %errorlevel%
+)
