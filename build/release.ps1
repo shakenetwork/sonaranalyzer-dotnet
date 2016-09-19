99c872b760d16542ce5da8987d73fff1e14cd35d
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-09-19 12:04:27 +0200

    Fix release script to generate both C# and VB.NET packages

--- build/release.ps1
+++ build/release.ps1
@@ -1,51 +1,55 @@
-$ErrorActionPreference = "Stop"
+function CreateRelease([string] $productName, [string] $version, [string] $expectedSha1, [string] $releaseVersion)
+{
+    # delete NuGet file if exists
+    If (Test-Path "$productName.$version.nupkg"){
+        Remove-Item "$productName.$version.nupkg"
+    }
 
-$version = $(env:BUILD_NAME)
-$expectedSha1 = $(env:SHA1)
-$releaseVersion = $(env:RELEASE_NAME)
+    # download nuget package
+    $url = "https://repox.sonarsource.com/sonarsource-nuget-releases/$productName.$version.nupkg"
+    Invoke-WebRequest -UseBasicParsing -Uri "$url" -OutFile "$productName.$version.nupkg"
 
-$productName = "SonarAnalyzer.CSharp"
+    # create working folder
+    $currentDir=(Get-Item -Path ".\" -Verbose).FullName
+    $workDir=$currentDir + "\$productName"
+    New-Item $workDir -type directory -force
+    Remove-Item "$workDir\*" -recurse
 
-# download nuget package
-$url = "https://repox.sonarsource.com/sonarsource-nuget-releases/$productName.$version.nupkg"
-Invoke-WebRequest -UseBasicParsing -Uri "$url" -OutFile "$productName.$version.nupkg"
+    # unzip nuget package
+    $zipName="$productName.$version.zip"
+    Move-Item "$productName.$version.nupkg" $zipName -force
+    $shell_app=new-object -com shell.application
+    $destination = $shell_app.NameSpace($workDir)
+    $zip_file = $shell_app.NameSpace("$currentDir\$zipName")
+    Write-Host "Unzipping $workDir\$zipName"
+    $destination.CopyHere($zip_file.Items(), 0x14) 
 
-$currentDir=(Get-Item -Path ".\" -Verbose).FullName
-$workDir=$currentDir + "\$productName"
-New-Item $workDir -type directory
-Remove-Item "$workDir\*" -recurse
+    # get sha1
+    $productversion=ls $workDir\analyzers\SonarAnalyzer.dll | % { $_.versioninfo.productversion }
+    $sha1=$productversion.Substring($productversion.LastIndexOf('Sha1:')+5)
+    if ($sha1 -ne $expectedSha1){
+        Write-Host "SHA1 doesn't match expected"
+        return
+    }
 
-# unzip nuget package
-$zipName="$productName.$version.zip"
-Move-Item "$productName.$version.nupkg" $zipName -force
-$shell_app=new-object -com shell.application
-$destination = $shell_app.NameSpace($workDir)
-$zip_file = $shell_app.NameSpace("$currentDir\$zipName")
-Write-Host "Unzipping $workDir\$zipName"
-$destination.CopyHere($zip_file.Items(), 0x14) 
+    # change content of nuspec file
+    (Get-Content $workDir\$productName.nuspec) -replace "<version>$version</version>", "<version>$releaseVersion</version>" | Set-Content $workDir\$productName.nuspec
 
-# get sha1
-$productversion=ls $workDir\analyzers\SonarAnalyzer.dll | % { $_.versioninfo.productversion }
-$sha1=$productversion.Substring($productversion.LastIndexOf('Sha1:')+5)
-if ($sha1 -ne $expectedSha1){
-    Write-Host "SHA1 doesn't match expected"
-    exit 1
+    $additionalContent = "<files>
+     <file src=""analyzers\SonarAnalyzer.dll"" target=""analyzers"" />
+     <file src=""analyzers\$productName.dll"" target=""analyzers"" />
+     <file src=""tools\*.ps1"" target=""tools\"" />
+    </files>"
+
+    $fileContent = (Get-Content $workDir\$productName.nuspec) -replace "</metadata>", "</metadata>$additionalContent"
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+    [System.IO.File]::WriteAllLines("$workDir\$productName.nuspec", $fileContent, $Utf8NoBomEncoding)
+
+    # repack with the new name
+    &$(env:NUGET_PATH) pack $workDir\$productName.nuspec
 }
 
-# change content of nuspec file
-(Get-Content $workDir\$productName.nuspec) -replace "<version>$version</version>", "<version>$releaseVersion</version>" | Set-Content $workDir\$productName.nuspec
-
-$additionalContent = "<files>
- <file src=""analyzers\SonarAnalyzer.dll"" target=""analyzers"" />
- <file src=""analyzers\$productName.dll"" target=""analyzers"" />
- <file src=""tools\*.ps1"" target=""tools\"" />
-</files>"
-
-$fileContent = (Get-Content $workDir\$productName.nuspec) -replace "</metadata>", "</metadata>$additionalContent"
-$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
-[System.IO.File]::WriteAllLines("$workDir\$productName.nuspec", $fileContent, $Utf8NoBomEncoding)
-
-# repack with the new name
-$(env:NUGET_PATH) pack $workDir\$productName.nuspec
+CreateRelease -productName "SonarAnalyzer.CSharp" -version $(env:BUILD_NAME) -expectedSha1 $(env:SHA1) -releaseVersion $(env:RELEASE_NAME)
+CreateRelease -productName "SonarAnalyzer.VisualBasic" -version $(env:BUILD_NAME) -expectedSha1 $(env:SHA1) -releaseVersion $(env:RELEASE_NAME)
 
 # push to repox
