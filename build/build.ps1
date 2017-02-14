a0ed0aae93d34a86ed8f083e5db0f3dc5bd37f86
Thomas Vérin <thomas.verin@steria.ch>
2017-02-14 17:46:44 +0100

    BUILD: Upload artifacts for PR

--- build/build.ps1
+++ build/build.ps1
@@ -14,9 +14,22 @@ function testExitCode(){
     unzip -o .\MSBuild.SonarQube.Runner.zip
     testExitCode
 
-function triggerQA(
+function setVersion(
     [string] $version)
 {
+    #generate build version from the build number
+    $buildversion="$env:BUILD_NUMBER"
+    
+    $branchName = "$env:GITHUB_BRANCH"
+    $sha1 = "$env:GIT_SHA1"
+
+    #Append build number to the versions
+    (Get-Content .\build\Version.props) -replace '<NugetVersion>\$\(MainVersion\)</NugetVersion>', "<NugetVersion>`$(MainVersion).$buildversion</NugetVersion>" | Set-Content .\build\Version.props
+    (Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
+    (Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
+    & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
+    testExitCode
+    
     #write version to property file
     $s="VERSION=$version"
     Write-Host "$s"
@@ -27,11 +40,50 @@ function triggerQA(
         $utf8 = New-Object System.Text.UTF8Encoding $false
         [IO.File]::WriteAllText($_, $contents, $utf8)
     }
-}    
+}  
+
+function generatePackages()
+{
+    #Generate the XML descriptor files for the C# plugin
+    pushd .\src\SonarAnalyzer.RuleDescriptorGenerator\bin\Release
+    .\SonarAnalyzer.RuleDescriptorGenerator.exe cs
+    .\SonarAnalyzer.RuleDescriptorGenerator.exe vbnet
+    popd
+    #generate packages
+    $files = Get-ChildItem .\src -recurse *.nuspec
+    foreach ($file in $files) {
+        $output = $file.directoryname+"\bin\Release"
+        & $env:NUGET_PATH pack $file.fullname -NoPackageAnalysis -OutputDirectory $output
+        testExitCode
+    }
+}
+
+function uploadToRepox(
+    [string] $version)
+{
+    cd build\poms
+    write-host -f green  "set version $version in pom.xml"
+    $command = "mvn versions:set -DgenerateBackupPoms=false -DnewVersion='$version'"
+    iex $command
+    write-host -f green  "set version $version in env VAR PROJECT_VERSION for artifactory buildinfo metadata"
+    $env:PROJECT_VERSION=$version
+    write-host -f green  "set the buildnumber to this job build number"
+    $env:BUILD_ID=$env:BUILD_NUMBER
+    write-host -f green  "Deploy to repox with $version"    
+    $command = 'mvn deploy -Pdeploy-sonarsource -B -e -V'
+    iex $command
+}  
 
 if ($env:IS_PULLREQUEST -eq "true") { 
     write-host -f green "in a pull request"
 
+    #get version number
+    [xml]$versionProps = Get-Content .\build\Version.props
+    $version = $versionProps.Project.PropertyGroup.MainVersion+".$buildversion"
+    
+    setVersion -version $version
+
+    #start analysis
     .\MSBuild.SonarQube.Runner begin /k:sonaranalyzer-csharp-vbnet /n:"SonarAnalyzer for C#" /v:latest `
         /d:sonar.host.url=$env:SONAR_HOST_URL `
         /d:sonar.login=$env:SONAR_TOKEN `
@@ -44,19 +96,27 @@ if ($env:IS_PULLREQUEST -eq "true") {
 
     & $env:NUGET_PATH restore .\SonarAnalyzer.sln
     testExitCode
-    & $env:MSBUILD_PATH .\SonarAnalyzer.sln /t:rebuild /p:Configuration=Release /p:DeployExtension=false
+    & $env:MSBUILD_PATH .\SonarAnalyzer.sln /p:configuration=Release /p:DeployExtension=false /p:ZipPackageCompressionLevel=normal /v:m /p:defineConstants=SignAssembly /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=$env:CERT_PATH
     testExitCode
 
+    #end analysis
     .\MSBuild.SonarQube.Runner end /d:sonar.login=$env:SONAR_TOKEN
     testExitCode
 
-    #generate build version from the build number
-    $buildversion="$env:BUILD_NUMBER"
-    #get version number
-    [xml]$versionProps = Get-Content .\build\Version.props
-    $version = $versionProps.Project.PropertyGroup.MainVersion+".$buildversion"
-        
-    triggerQA -version $version
+    #generate packages
+    generatePackages
+
+    #upload packages
+    $files = Get-ChildItem src -recurse *.nupkg
+    foreach ($file in $files) {    
+        #compute artifact name from filename
+        $artifact=$file.name.replace($file.extension,"").replace(".$version","")
+        $filePath=$file.fullname
+        write-host -f green "XXX DEBUG: $artifact"
+        (Get-Content .\build\poms\$artifact\pom.xml) -replace "file-$artifact", "$filePath" | Set-Content .\build\poms\$artifact\pom.xml          
+    }
+    #upload to maven repo  
+    uploadToRepox -version $version
 
 } else {
     if (($env:GITHUB_BRANCH -eq "master") -or ($env:GITHUB_BRANCH -eq "refs/heads/master")) {
@@ -69,24 +129,12 @@ if ($env:IS_PULLREQUEST -eq "true") {
         $apikey = $env:ARTIFACTORY_DEPLOY_USERNAME+":"+$env:ARTIFACTORY_DEPLOY_PASSWORD
         & $env:NUGET_PATH setapikey $apikey -Source repox
         testExitCode
-        
-        #generate build version from the build number
-        $buildversion="$env:BUILD_NUMBER"
-        
-        $branchName = "$env:GITHUB_BRANCH"
-        $sha1 = "$env:GIT_SHA1"
-
-        #Append build number to the versions
-        (Get-Content .\build\Version.props) -replace '<NugetVersion>\$\(MainVersion\)</NugetVersion>', "<NugetVersion>`$(MainVersion).$buildversion</NugetVersion>" | Set-Content .\build\Version.props
-        (Get-Content .\build\Version.props) -replace '<AssemblyFileVersion>\$\(MainVersion\)\.0</AssemblyFileVersion>', "<AssemblyFileVersion>`$(MainVersion).$buildversion</AssemblyFileVersion>" | Set-Content .\build\Version.props
-        (Get-Content .\build\Version.props) -replace '<AssemblyInformationalVersion>Version:\$\(AssemblyFileVersion\) Branch:not-set Sha1:not-set</AssemblyInformationalVersion>', "<AssemblyInformationalVersion>Version:`$(AssemblyFileVersion) Branch:$branchName Sha1:$sha1</AssemblyInformationalVersion>" | Set-Content .\build\Version.props
-        & $env:MSBUILD_PATH  .\build\ChangeVersion.proj
-        testExitCode
 
         #get version number
         [xml]$versionProps = Get-Content .\build\Version.props
         $version = $versionProps.Project.PropertyGroup.MainVersion+".$buildversion"
 
+        setVersion -version $version
 
         #start analysis
         .\MSBuild.SonarQube.Runner begin /k:sonaranalyzer-csharp-vbnet /n:"SonarAnalyzer for C#" /v:master `
@@ -104,23 +152,9 @@ if ($env:IS_PULLREQUEST -eq "true") {
         .\MSBuild.SonarQube.Runner end /d:sonar.login=$env:SONAR_TOKEN
         testExitCode
 
-
-        #Generate the XML descriptor files for the C# plugin
-        pushd .\src\SonarAnalyzer.RuleDescriptorGenerator\bin\Release
-        .\SonarAnalyzer.RuleDescriptorGenerator.exe cs
-        .\SonarAnalyzer.RuleDescriptorGenerator.exe vbnet
-        popd
-
         #generate packages
-        $files = Get-ChildItem .\src -recurse *.nuspec
-        foreach ($file in $files) {
-            $output = $file.directoryname+"\bin\Release"
-            & $env:NUGET_PATH pack $file.fullname -NoPackageAnalysis -OutputDirectory $output
-            testExitCode
-        }
+        generatePackages
         
-        triggerQA -version $version
-
         #upload packages
         $files = Get-ChildItem src -recurse *.nupkg
         foreach ($file in $files) {    
@@ -132,18 +166,9 @@ if ($env:IS_PULLREQUEST -eq "true") {
             $filePath=$file.fullname
             (Get-Content .\build\poms\$artifact\pom.xml) -replace "file-$artifact", "$filePath" | Set-Content .\build\poms\$artifact\pom.xml          
         }
-        #upload to maven repo        
-        cd build\poms
-        write-host -f green  "set version $version in pom.xml"
-        $command = "mvn versions:set -DgenerateBackupPoms=false -DnewVersion='$version'"
-        iex $command
-        write-host -f green  "set version $version in env VAR PROJECT_VERSION for artifactory buildinfo metadata"
-        $env:PROJECT_VERSION=$version
-        write-host -f green  "set the buildnumber to this job build number"
-        $env:BUILD_ID=$env:BUILD_NUMBER
-        write-host -f green  "Deploy to repox with $version"    
-        $command = 'mvn deploy -Pdeploy-sonarsource -B -e -V'
-        iex $command
+        #upload to maven repo  
+        uploadToRepox -version $version      
+        
     } else {
         write-host -f green "not on master"
     }
