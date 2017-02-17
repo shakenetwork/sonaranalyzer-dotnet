c30a5fe808d054a5f4d57fdc26aea047b4e5989a
Amaury Levé <amaury.leve@sonarsource.com>
2017-02-17 09:32:46 +0100

    Update minor version

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -21,9 +21,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.22.0")]
-[assembly: AssemblyFileVersion("1.22.0.0")]
-[assembly: AssemblyInformationalVersion("Version:1.22.0.0 Branch:not-set Sha1:not-set")]
+[assembly: AssemblyVersion("1.23.0")]
+[assembly: AssemblyFileVersion("1.23.0.0")]
+[assembly: AssemblyInformationalVersion("Version:1.23.0.0 Branch:not-set Sha1:not-set")]
 
 [assembly: AssemblyConfiguration("")]
 
